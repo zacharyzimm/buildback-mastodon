b18f56943578d4e2e5efbfcc15eaa561406017d6
zacharyzimm <105132282+zacharyzimm@users.noreply.github.com>
2025-01-30 14:40:38 -0800

    remove option to join another server on create account click

--- app/javascript/mastodon/features/closed_registrations_modal/index.jsx
+++ app/javascript/mastodon/features/closed_registrations_modal/index.jsx
@@ -56,17 +56,6 @@ class ClosedRegistrationsModal extends ImmutablePureComponent {
             <h3><FormattedMessage id='interaction_modal.on_this_server' defaultMessage='On this server' /></h3>
             {closedRegistrationsMessage}
           </div>
-
-          <div className='interaction-modal__choices__choice'>
-            <h3><FormattedMessage id='interaction_modal.on_another_server' defaultMessage='On a different server' /></h3>
-            <p className='prose'>
-              <FormattedMessage
-                id='closed_registrations.other_server_instructions'
-                defaultMessage='Since Mastodon is decentralized, you can create an account on another server and still interact with this one.'
-              />
-            </p>
-            <a href='https://joinmastodon.org/servers' className='button button--block'><FormattedMessage id='closed_registrations_modal.find_another_server' defaultMessage='Find another server' /></a>
-          </div>
         </div>
       </div>
     );
